ab526c67aebcad05e0a9bf57c2c4f0d0e4265b33
Isaac Buckman <isaacbuckman4@gmail.com>
2018-03-13 00:40:25 -0400

    create service that overlays color

---
+++ ColorPal/app/src/main/java/com/tsa/hths/colorpal/FilterScreen.java
@@ -0,0 +1,31 @@
+package com.tsa.hths.colorpal;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class FilterScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_screen);
+        toggleService();
+    }
+
+    private void toggleService(){
+        Intent intent=new Intent(this, OverlayService.class);
+        if(!stopService(intent)){
+            if (android.os.Build.VERSION.SDK_INT >= 23 && !Settings.canDrawOverlays(this)) {   //Android M Or Over
+                Intent i = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                startActivityForResult(i, 1);
+                return;
+            } else {
+                startService(intent);
+            }
+        }
+    }
+}
