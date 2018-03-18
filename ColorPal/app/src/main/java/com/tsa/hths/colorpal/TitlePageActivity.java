b32ca7a08507dda72a16c05bde514b19a873f925
Arvind Yalavarti <yalavartiarvind@gmail.com>
2018-03-17 20:53:13 -0400

    Front End UI Changes
    Added color scheme to app
    Uploaded logo
    Updated buttons

--- ColorPal/app/src/main/java/com/tsa/hths/colorpal/TitlePageActivity.java
+++ ColorPal/app/src/main/java/com/tsa/hths/colorpal/TitlePageActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.Fragment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class TitlePageActivity extends FragmentActivity {
 
@@ -18,6 +19,9 @@ public class TitlePageActivity extends FragmentActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.title_page_layout);
+
+        //ImageView app_logo = (ImageView) findViewById(R.id.app_logo);
+        //app_logo.setImageResource(R.drawable.app_logo);
 
         mDiagnosticTestButton = (Button) findViewById(R.id.title_page_diagnostic_button);
         mDiagnosticTestButton.setOnClickListener(new View.OnClickListener() {
