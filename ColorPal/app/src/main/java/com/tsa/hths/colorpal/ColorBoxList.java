ab03afac008b0b0789e20405e6a0482e32f2cdcc
Chris Yin <chyin@ctemc.org>
2018-03-18 21:52:06 -0400

    it moves

--- ColorPal/app/src/main/java/com/tsa/hths/colorpal/ColorBoxList.java
+++ ColorPal/app/src/main/java/com/tsa/hths/colorpal/ColorBoxList.java
@@ -6,12 +6,14 @@ import java.util.Collections;
 public class ColorBoxList {
     private ArrayList<Integer> mOrigColors;
     private ArrayList<Integer> mColors;
+    private ArrayList<Integer> mErrors;
     private int mLastClicked;
 
     public ColorBoxList(ArrayList<Integer> colors)
     {
         mOrigColors = new ArrayList<Integer>();
         mColors = new ArrayList<Integer>();
+        mErrors = new ArrayList<Integer>();
         mLastClicked = -1;
 
         for(Integer i : colors)
@@ -21,10 +23,11 @@ public class ColorBoxList {
         }
     }
 
-    public void setColors(ArrayList<Integer> colors)
+    public void setColors(ArrayList<Integer> colors) // change dataset of colors
     {
         mOrigColors.clear();
         mColors.clear();
+        mLastClicked = -1;
 
         for(Integer i : colors)
         {
@@ -33,45 +36,123 @@ public class ColorBoxList {
         }
     }
 
-    public void handleClick(int pos)
+    public void handleClick(int pos) // select color, swap colors, or do nothing
     {
-        if(pos == 0 || pos == mColors.size() - 1)
+        if(pos == 0 || pos == mColors.size() - 1) // do nothing (selected an anchor value)
         {
             mLastClicked = -1; // reset
             return;
         }
-        else if(mLastClicked == -1)
+        else if(mLastClicked == -1) // select color
         {
             mLastClicked = pos;
         }
-        else
+        else // swap colors
         {
             swap(mLastClicked, pos);
             mLastClicked = -1;
         }
     }
 
-    public void shuffleColors()
+    public void shuffleColors() // shuffle all colors (except first and last ones)
     {
         Collections.shuffle(mColors.subList(1, mColors.size() - 1)); // do not include first and last elements in shuffle
     }
 
-    public void swap(int a, int b)
+    public void swap(int a, int b) // swap colors at pos a and pos b
     {
         int temp = mColors.get(a);
         mColors.set(a, mColors.get(b));
         mColors.set(b, temp);
     }
 
-    public int getScore()
+    public void addErrors() // add errors to list of errors
     {
         int score = 0;
-        for(Integer i : mColors)
+        for(Integer i : mOrigColors)
         {
-            score += Math.pow(mColors.indexOf(i) - mOrigColors.indexOf(i), 2);
+            mErrors.add((int) Math.pow(mColors.indexOf(i) - mOrigColors.indexOf(i), 2));
         }
-        return score;
     }
+
+    public int getRedError() // calculate red error
+    {
+        if(mErrors.size() < 88)
+        {
+            return -1;
+        }
+        else
+        {
+            int red1 = 0;
+            for(int i = 63; i <= 71; i++)
+            {
+                red1 += mErrors.get(i);
+            }
+            red1 /= 9; // average from red sector 1
+
+            int red2 = 0;
+            for(int i = 15; i <= 24; i++)
+            {
+                red2 += mErrors.get(i);
+            }
+            red2 /= 10; // average from red sector 2
+
+            return red1 * red2; // find product
+        }
+    }
+
+    public int getGreenError() // calculate green error
+    {
+        if(mErrors.size() < 88)
+        {
+            return -1;
+        }
+        else
+        {
+            int green1 = 0;
+            for(int i = 56; i <= 62; i++)
+            {
+                green1 += mErrors.get(i);
+            }
+            green1 /= 7;  // average from green sector 1
+
+            int green2 = 0;
+            for(int i = 11; i <= 19; i++)
+            {
+                green2 += mErrors.get(i);
+            }
+            green2 /= 9;  // average from green sector 2
+
+            return green1 * green2;  // find product
+        }
+    }
+
+    public int getBlueError()  // calculate blue error
+    {
+        if(mErrors.size() < 88)
+        {
+            return -1;
+        }
+        else
+        {
+            int blue1 = 0;
+            for(int i = 45; i <= 53; i++)
+            {
+                blue1 += mErrors.get(i);
+            }
+            blue1 /= 9;  // average from blue sector 1
+
+            int blue2 = 0;
+            for(int i = 1; i <= 6; i++)
+            {
+                blue2 += mErrors.get(i);
+            }
+            blue2 /= 6;  // average from blue sector 2
+
+            return blue1 * blue2;  // find product
+        }
+    }
+
 
     public int getColorAtPos(int pos)
     {
