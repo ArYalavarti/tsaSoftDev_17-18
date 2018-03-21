167337a1fefc109ee8c5ad932a8d3174146cf0ae
Isaac Buckman <isaacbuckman4@gmail.com>
2018-03-21 19:59:48 -0400

    fix crash when upload filtered image

--- ColorPal/app/src/main/java/com/tsa/hths/colorpal/ImageFilter.java
+++ ColorPal/app/src/main/java/com/tsa/hths/colorpal/ImageFilter.java
@@ -8,13 +8,13 @@ public class ImageFilter {
     public ImageFilter() {
     }
 
-    public Bitmap filterImage(Bitmap image, String colorBlindnessType) {
-
+    public Bitmap filterImage(Bitmap bmp, String colorBlindnessType) {
+        Bitmap bmpCopy = bmp.copy(bmp.getConfig(), true);
         if (colorBlindnessType.equals("RED") || colorBlindnessType.equals("GREEN")) {
 
-            for (int x = 0; x < image.getWidth(); x++) {
-                for (int y = 0; y < image.getHeight(); y++) {
-                    int colorInitial = image.getPixel(x, y);
+            for (int x = 0; x < bmpCopy.getWidth(); x++) {
+                for (int y = 0; y < bmpCopy.getHeight(); y++) {
+                    int colorInitial = bmpCopy.getPixel(x, y);
 
                     int a = Color.alpha(colorInitial);
                     int r = Color.red(colorInitial);
@@ -22,13 +22,13 @@ public class ImageFilter {
                     int b = Color.blue(colorInitial);
 
                     int colorOutput = Color.rgb(b, g, r);
-                    image.setPixel(x, y, colorOutput);
+                    bmpCopy.setPixel(x, y, colorOutput);
                 }
             }
         } else if (colorBlindnessType.equals("BLUE")) {
-            for (int x = 0; x < image.getWidth(); x++) {
-                for (int y = 0; y < image.getHeight(); y++) {
-                    int colorInitial = image.getPixel(x, y);
+            for (int x = 0; x < bmpCopy.getWidth(); x++) {
+                for (int y = 0; y < bmpCopy.getHeight(); y++) {
+                    int colorInitial = bmpCopy.getPixel(x, y);
 
                     int a = Color.alpha(colorInitial);
                     int r = Color.red(colorInitial);
@@ -36,10 +36,10 @@ public class ImageFilter {
                     int b = Color.blue(colorInitial);
 
                     int colorOutput = Color.rgb(255 - b, 255 - g, 255 - r);
-                    image.setPixel(x, y, colorOutput);
+                    bmpCopy.setPixel(x, y, colorOutput);
                 }
             }
         }
-        return image;
+        return bmpCopy;
     }
 }
